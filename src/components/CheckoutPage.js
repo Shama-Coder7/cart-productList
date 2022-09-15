22ba8664c42ea3fe3e1fa7b5ee7b441599e03453
Shama Parveen <85455446+Shama-Coder7@users.noreply.github.com>
2022-09-15 20:56:34 +0530

    Add files via upload

---
+++ src/components/CheckoutPage.js
@@ -0,0 +1,10 @@
+
+const CheckoutPage = () => {
+  return (
+    <div>
+        <h1 style={{color: 'green', margin: '15%', fontSize: '30px', fontFamily:'cursive'}}>Thanks for Shopping</h1>
+    </div>
+  )
+}
+
+export default CheckoutPage
